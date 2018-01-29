efe69afacce0c3114cb86c14aa85083b707f29ca
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-29 10:23:57 +0200

    Update index.js

--- mod2/index.js
+++ mod2/index.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiModulesDemo extends Component {
+class MultiModulesDemo2 extends Component {
 	
   render(){
 	  
@@ -13,4 +13,4 @@ class MultiModulesDemo extends Component {
   }
 }
 
-export default ModuleDemo;
+export default MultiModulesDemo2;
